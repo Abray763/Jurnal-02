1a145b805701a2afe91b331843337438a639221a
Abdul Raihan <abraykun@gmail.com>
2021-09-27 21:30:30 +0800

    enter

---
+++ LOL/src/Ima.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ima {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nama :");
+        String a = sc.nextLine();
+        System.out.print("Nim :");
+        String b = sc.nextLine();
+        System.out.print("Ipk :");
+        float c = sc.nextFloat();
+     
+        System.out.println("Nama\t : "+ a);
+        System.out.println("Nim\t :  "+ b);
+        System.out.println("Ipk\t : "+ c);
+    }
+}
+
+
